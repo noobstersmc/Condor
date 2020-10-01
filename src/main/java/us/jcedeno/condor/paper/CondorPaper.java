f6cfcda051e258d437c2cefc973166e0512b2211
InfinityZ25 <henixceo@gmail.com>
2020-10-01 11:10:27 -0400

    Migrated to gradle

---
+++ src/main/java/us/jcedeno/condor/paper/CondorPaper.java
@@ -0,0 +1,16 @@
+package us.jcedeno.condor.paper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * CondorPaper
+ */
+public class CondorPaper extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+        System.out.println("Condor has re-re-landed");
+        
+    }
+    
+}
